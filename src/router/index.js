c74bba4d967b4f208c910752daf2f522db1dbdc8
Young <634046906@qq.com>
2021-01-25 19:01:19 +0800

    feat: 轮播图组件(有问题)

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Recommend from 'components/recommend/recommend.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    name: 'Recommend',
+    component: Recommend
+  },
+  {
+    path: '/rank',
+    name: 'Rank',
+    component: () => import('components/rank/rank.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import('components/search/search.vue')
+  },
+  {
+    path: '/singer',
+    name: 'Singer',
+    component: () => import('components/singer/singer.vue')
+  },
 ]
 
 const router = new VueRouter({
